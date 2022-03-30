b2b1030aa9d8cd9a0d215207de8ae1d2e9046303
Mayvis <kevin73911@gmail.com>
2022-03-30 16:49:56 +0800

    add websocket mimic features for test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
-import { ref } from "vue";
+import { ref, watch } from "vue";
 import Editor from "@/components/Editor.vue";
+import { useToggle } from "@vueuse/core";
 
 const dummyData = ref([
   {
@@ -25,19 +26,38 @@ function handleUpdateData({ segment, transcript }) {
   dummyData.value[segment].transcript = transcript;
 }
 
-let dev = false;
+const [dev, toggleDev] = useToggle(false);
 
-if (dev) {
-  setInterval(() => {
-    dummyData.value.push({
-      segment: dummyData.value.length,
-      transcript: "哈囉你好嗎顆顆顆",
-    });
-  }, 3000);
-}
+let timer = null;
+
+watch(dev, (val) => {
+  if (val) {
+    timer = setInterval(() => {
+      dummyData.value.push({
+        segment: dummyData.value.length,
+        transcript: "哈囉你好嗎顆顆顆",
+      });
+    }, 3000);
+  } else {
+    clearInterval(timer);
+  }
+});
 </script>
 
 <template>
+  <div flex justify-center items-center mb-4>
+    <button
+      border-none
+      text-white
+      p-12px
+      cursor-pointer
+      bg-sky-600
+      hover:bg-sky-700
+      @click="toggleDev()"
+    >
+      模擬 Websocket: {{ dev ? "關閉" : "開啟" }}
+    </button>
+  </div>
   <Editor :data="dummyData" @update-data="handleUpdateData" />
 </template>
 
