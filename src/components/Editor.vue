6fd81909982631c3a4e6a0607f5e47a8ed062601
Mayvis <kevin73911@gmail.com>
2022-03-30 15:22:47 +0800

    build handle cut method

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -271,12 +271,165 @@ async function handleKeydown(event) {
 }
 
 async function handleCut() {
+  // using copy instead cut to mimic cutting behavior
   document.execCommand("copy");
+
+  const sel = document.getSelection();
+  const range = sel.getRangeAt(0);
+
+  if (range.startContainer === range.endContainer) {
+    // handle cutting same line and same container situation ex: "hello" -> "heo"
+    const { startOffset, endOffset, startContainer, commonAncestorContainer } =
+      range;
+    const parent = commonAncestorContainer.parentElement;
+    const segment = +parent.dataset.segment;
+
+    let transcript = "";
+    let index = 0;
+    if (parent.childNodes.length === 1) {
+      // because is same line and same container, just remove string
+      transcript = removeString(parent.innerHTML, startOffset, endOffset);
+    } else {
+      // this situation is the same container but different line, so have to calculate the length of each node
+      // ex: "呵<br>呵呵<br>你好嗎" -> "呵<br><br>你好嗎"
+      for (const e of parent.childNodes.entries()) {
+        if (e[1] === startContainer) {
+          index = e[0];
+          // handle "呵<br>呵呵<br>你好嗎" -> "呵<br>呵<br>你好嗎" need to calculate words
+          if (startOffset + endOffset > e[1].length) {
+            // the total has bigger then the node length, so just calculate the index 0 to startOffset
+            transcript += e[1].data.substring(0, startOffset);
+          } else {
+            transcript += removeString(e[1].data, startOffset, endOffset);
+          }
+        } else {
+          if (e[1].data) {
+            transcript += e[1].data;
+          } else if (e[1].outerHTML) {
+            transcript += e[1].outerHTML;
+          }
+        }
+      }
+    }
+
+    emits("updateData", {
+      segment,
+      transcript,
+    });
+
+    await nextTick();
+
+    const r = new Range();
+    r.collapse(false);
+
+    if (parent.childNodes.length === 1) {
+      r.setStart(sel.anchorNode.childNodes[0], startOffset);
+    } else {
+      r.setStart(sel.anchorNode.childNodes[index], startOffset);
+    }
+
+    sel.removeAllRanges();
+    sel.addRange(r);
+  } else if (
+    range.startContainer.parentElement === range.endContainer.parentElement
+  ) {
+    // handle cutting same container but different line ex: [text, br, text] -> [text]
+    let replaceHTML = "";
+    let between = false;
+    let index = 0;
+    for (const e of range.commonAncestorContainer.childNodes.entries()) {
+      if (e[1] === range.startContainer) {
+        between = true;
+        index = e[0];
+        replaceHTML += e[1].data.substring(0, range.startOffset);
+      } else if (e[1] === range.endContainer) {
+        between = false;
+        replaceHTML += e[1].data.substring(
+          range.endOffset,
+          range.endContainer.length
+        );
+      } else if (!between) {
+        // if between the startContainer and endContainer, the node will be removed, otherwise just append the node
+        if (e[1].nodeType === 1) {
+          replaceHTML += e[1].outerHTML;
+        } else if (e[1].nodeType === 3) {
+          replaceHTML += e[1].data;
+        }
+      }
+    }
+
+    const { startOffset } = range;
+
+    emits("updateData", {
+      segment: +range.commonAncestorContainer.dataset.segment,
+      transcript: replaceHTML,
+    });
+
+    await nextTick();
+
+    const r = new Range();
+    r.collapse(false);
+    r.setStart(sel.anchorNode.childNodes[index], startOffset);
+
+    sel.removeAllRanges();
+    sel.addRange(r);
+  } else {
+    const replaceHTML = [];
+    let start = null;
+    for (const e of range.commonAncestorContainer.childNodes.entries()) {
+      if (e[1] === range.startContainer.parentElement) {
+        start = +range.startContainer.parentElement.dataset.segment;
+
+        const transcript = range.startContainer.textContent.substring(
+          0,
+          range.startOffset
+        );
+
+        replaceHTML.push({
+          segment: start,
+          transcript,
+        });
+      } else if (e[1] === range.endContainer.parentElement) {
+        const transcript = range.endContainer.textContent.substring(
+          range.endOffset,
+          range.endContainer.textContent.length
+        );
+
+        replaceHTML.push({
+          segment: +range.endContainer.parentElement.dataset.segment,
+          transcript,
+        });
+
+        break;
+      } else if (start !== null) {
+        replaceHTML.push({
+          segment: ++start,
+          transcript: "",
+        });
+      }
+    }
+
+    for (let i = 0; i < replaceHTML.length; i++) {
+      const { segment, transcript } = replaceHTML[i];
+
+      emits("updateData", {
+        segment,
+        transcript,
+      });
+
+      await nextTick();
+    }
+
+    const r = new Range();
+    r.collapse(false);
+    r.setStart(sel.anchorNode.childNodes[0], replaceHTML[0].transcript.length);
+
+    sel.removeAllRanges();
+    sel.addRange(r);
+  }
 }
 
-async function handlePaste() {
-  //
-}
+async function handlePaste() {}
 
 async function handleCompositionend() {
   const sel = document.getSelection();
