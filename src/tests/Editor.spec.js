220f3c39ac4e58bbb56aa18a270d409bbdeb652e
Mayvis <kevin73911@gmail.com>
2022-03-31 11:56:52 +0800

    add global sanitize

--- src/tests/Editor.spec.js
+++ src/tests/Editor.spec.js
@@ -2,18 +2,21 @@ import { mount } from "@vue/test-utils";
 import Editor from "../components/Editor.vue";
 import sanitize from "sanitize-html";
 
-beforeAll(() => {
-  global.$sanitize = sanitize;
-});
-
 test("mount component", async () => {
   expect(Editor).toBeTruthy();
 
   const wrapper = mount(Editor, {
     props: {
-      data: {
-        segment: 0,
-        transcript: "This is a transcript",
+      data: [
+        {
+          segment: 0,
+          transcript: "This is a transcript",
+        },
+      ],
+    },
+    global: {
+      mocks: {
+        $sanitize: (html) => sanitize(html),
       },
     },
   });
